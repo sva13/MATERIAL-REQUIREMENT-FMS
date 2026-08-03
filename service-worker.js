32f634621abcbd344718e8011a5eca5b48e289ae
sva13 <info@svacouture.com>
2026-08-03 18:15:31 +0530

    Add basic service worker functionality
    Implement basic service worker with install, activate, and fetch event listeners.

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // network-first (always fresh data)
+});
